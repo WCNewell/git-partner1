2c69e42554d491e7c1af58b657bcf3f1e06a4792
SavannahAdamsss <savannah.adams0@gmail.com>
2018-04-11 11:19:51 -0600

    ArrayOfPeople

--- app/array.js
+++ app/array.js
@@ -1,9 +1,20 @@
 var students = [{
-   name: ‘Clark’,
-   age: 45
- },
- {
-   name: ‘Savannah’,
-   age: 31
- }
-]
+  name: 'Will',
+  age: 27
+},
+{
+  name: 'Savannah',
+  age: 31
+}
+];
+
+
+var people = [{
+  name: 'John',
+  age: 2
+},
+{
+  name: 'Eric',
+  age: 64
+}
+];
